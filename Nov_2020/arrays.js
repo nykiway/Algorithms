420b94ff081fdc6e4f8d7645d19a5b10318a3b68
nykiway <nicoleoliviawiehe@gmail.com>
2020-11-24 14:55:26 -0700

    Add Enumerables

--- Nov_2020/arrays.js
+++ Nov_2020/arrays.js
@@ -52,4 +52,27 @@ Array.prototype.transpose = function() {
   return transposed;
 }
 
-console.log([[1,2],[4,5]].transpose()); // [[]]
+// console.log([[1,2],[4,5]].transpose()); // [[]]
+
+// ---------------------------------------------
+
+// Enumerables: My Each
+
+Array.prototype.myEach = function() {
+  let eached = [];
+
+  for (let i = 0; i < this.length; i++) {
+    eached.push(this[i]);
+  }
+
+  return eached;
+}
+
+console.log([1,2,3,4,5].myEach()); // => [1,2,3,4,5]
+
+// ---------------------------------------------
+
+// Enumerables: My Map
+
+Array.prototype.myMap = function() {
+}
