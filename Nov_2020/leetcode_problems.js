9da89ef4c1c6eeb1beed5108e1aa10e9c45153fc
nykiway <nicoleoliviawiehe@gmail.com>
2020-12-03 09:48:40 -0700

    Add runLengthEncoding solution

--- Nov_2020/leetcode_problems.js
+++ Nov_2020/leetcode_problems.js
@@ -36,3 +36,29 @@ var runningSum = function(nums) {
 }
 
 console.log(runningSum([1,2,3,4])); // [1,3,6,10]
+
+// RUN LENGTH ENCODING
+
+function runLengthEncoding(str) {
+  let encodedChars = [];
+	let currentRun = 1;
+	for (let i = 1; i < str.length; i++) {
+		let currentChar = str[i];
+		let prevChar = str[i - 1];
+		
+		if ((currentChar !== prevChar) || currentRun === 9) {
+			encodedChars.push(currentRun.toString());
+			encodedChars.push(prevChar);
+			currentRun = 0;
+		}
+		currentRun += 1;
+	}
+	
+	encodedChars.push(currentRun.toString());
+	encodedChars.push(str[str.length - 1]);
+	return encodedChars.join("")
+}
+
+console.log(runLengthEncoding("AAAAAAAAAAAAABBCCCCDD")); // 9A4A2B3C4D
+
+
